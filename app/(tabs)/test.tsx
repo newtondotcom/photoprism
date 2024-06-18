4a9e0c258fb1e484b94fff30257a1158ea30e8bb
newtondotcom <asphalt9fr@gmail.com>
2024-06-18 16:00:00 +0000

    test function

--- app/(tabs)/test.tsx
+++ app/(tabs)/test.tsx
@@ -8,20 +8,16 @@ export default function App () {
   const [albums, setAlbums] = useState<Album[] | null>(null);
   const [album, setAlbum] = useState<Album | null>(null);
 
-  const photoprism = new Photoprism("http://localhost:2342", "admin", "insecure");
-
-  useEffect(() => {
-    const fetchAlbums = async () => {
-      const albums = await photoprism.getAlbums();
-      setAlbums(albums);
-    };
-
-    fetchAlbums();
-  }, []);
+  const photoprism = new Photoprism("HOST", "USER", "PASSWORD");
 
   const createAlbum = async () => {
     const newAlbum = await photoprism.createAlbum({ Title: "test" });
     setAlbum(newAlbum);
+  };
+
+  const fetchAlbums = async () => {
+    const albums = await photoprism.getAlbums();
+    setAlbums(albums);
   };
 
   const getPermission = async () => {
@@ -38,9 +34,8 @@ export default function App () {
     }
 
     try {
-      const assets = await MediaLibrary.getAssetsAsync({ mediaType: 'photo' });
-
-      const uploadPromises = assets.assets.map(async (asset) => {
+        const assets = await MediaLibrary.getAssetsAsync({ mediaType: 'photo' });
+        const uploadPromises = assets.assets.map(async (asset) => {
         const uri = asset.uri;
         await photoprism.uploadPhotoToAlbum([album.UID!], uri);
       });
@@ -53,6 +48,13 @@ export default function App () {
     }
   };
 
+  const test = async () => {
+    const assets = await MediaLibrary.getAssetsAsync({ mediaType: 'photo' });
+    console.log(assets.assets[0]);
+    const albums = await photoprism.getAlbums();
+  }
+
+
   useEffect(() => {
     getPermission();
   }, []);
@@ -63,6 +65,7 @@ export default function App () {
       <View style={{ marginVertical: 50 }}>
       <Button title="Create Album" onPress={createAlbum} />
       <Button title="Sync Library" onPress={syncLibrary} />
+      <Button title="log photo" onPress={test} />
       </View>
     </View>
   );
