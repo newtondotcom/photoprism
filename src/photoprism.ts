20d43bef799a1e42f3478de0eca842c96dee34bb
newtondotcom <asphalt9fr@gmail.com>
2024-06-18 16:00:00 +0000

    no more ofetch + gettoken and createAlbum methos are working

--- src/photoprism.ts
+++ src/photoprism.ts
@@ -1,18 +1,18 @@
 import * as FileSystem from 'expo-file-system';
 import dayjs from 'dayjs';
 import { Album } from '@/src/types';
-import { ofetch } from 'ofetch';
 import { save, getValueFor } from '@/src/store';
 
+// OK - manque test dynamique token
 export async function getToken() {
-  let endpoint = getValueFor('endpoint');
-  let username = getValueFor('username');
-  let password = getValueFor('password');
-  let token = getValueFor('token');
-  let token_expiry_time = getValueFor('token_expiry_time');
+  let endpoint = await getValueFor('endpoint');
+  let username = await getValueFor('username');
+  let password = await getValueFor('password');
+  let token = await getValueFor('token');
+  let token_expiry_time = await getValueFor('token_expiry_time');
 
   if (!token_expiry_time) {
-    token_expiry_time = new Date();
+    token_expiry_time = new Date().toString();
   }
 
   if (token && dayjs().isBefore(token_expiry_time)) {
@@ -22,54 +22,57 @@ export async function getToken() {
   try {
     let data = {
       username,
-      password,
-      code: ""
+      password
     };
-
-    console.log("Token en cours de récupération : ",data);
+    
     let response;
     try {
-      response = await ofetch(`${endpoint}/api/v1/session`, {
+      response = await fetch(`${endpoint}/api/v1/session`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Cache-Control': 'no-store'
         },
         body: JSON.stringify(data)
       });
-      console.log("Token en cours de récupération : ",response);
+      console.log("Token en cours de récupération : ", response);
     } catch (e) {
-      console.log(e);
+      console.log("Fetch error:", e);
       throw e;
     }
 
     let responseData = await response.json();
-    console.log("Token réussi : ",responseData);
 
     var now = dayjs();
-    token_expiry_time = now.add(responseData.expires_in, 'second').toDate();
+    token_expiry_time = now.add(responseData.expires_in, 'second').toString();
     token = responseData.access_token;
     let user_id = responseData.user.UID;
 
-    save("token_expiry_time",token_expiry_time);
-    save("token",token);
-    save("user_id",user_id);
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
+    console.log("Token récupéré : ", token);
+    console.log("Token expiration : ", token_expiry_time);
+    console.log("User ID : ", user_id);
 
-  return token;
+
+    await save("token_expiry_time", token_expiry_time);
+    await save("token", token);
+    await save("user_id", user_id);
+    return "ok"
+  } catch (e) {
+    console.log("Error processing token:", e);
+    return "ko";
+  }
 }
 
+
 export async function getAlbums(count: Number = 24, offset: Number = 0, type: string = "album"): Promise<Album[] | null> {
-  let endpoint = getValueFor('endpoint');
-  let token = await getToken();
+  let endpoint = await getValueFor('endpoint');
+  let token = await getValueFor('token');
 
   let query_object = { count: count.toString(), offset: offset.toString(), type };
   let query_string = new URLSearchParams(query_object).toString();
-
+  //?${query_string}
   try {
-    let response = await ofetch(`${endpoint}/api/v1/albums?${query_string}`, {
+    let response = await fetch(`${endpoint}/api/v1/albums`, {
       method: 'GET',
       headers: {
         'X-Auth-Token': token
@@ -77,19 +80,22 @@ export async function getAlbums(count: Number = 24, offset: Number = 0, type: st
     });
 
     let responseData = await response.json();
+    console.log("Albums récupérés : ", responseData);
     return responseData;
   } catch (e) {
     console.log(e);
-    throw e;
+    console.log("Error fetching albums");
   }
 }
 
+
+// OK
 export async function createAlbum(album: Album): Promise<Album> {
-  let endpoint = getValueFor('endpoint');
-  let token = await getToken();
+  let endpoint = await getValueFor('endpoint');
+  let token = await getValueFor('token');
 
   try {
-    let response = await ofetch(`${endpoint}/api/v1/albums`, {
+    let response = await fetch(`${endpoint}/api/v1/albums`, {
       method: 'POST',
       headers: {
         'X-Auth-Token': token,
@@ -99,6 +105,7 @@ export async function createAlbum(album: Album): Promise<Album> {
     });
 
     let responseData = await response.json();
+    console.log("Album créé : ", responseData);
     return responseData;
   } catch (e) {
     console.log(e);
@@ -107,8 +114,8 @@ export async function createAlbum(album: Album): Promise<Album> {
 }
 
 export async function uploadPhotoToAlbum(albumUIDs: Array<string>, photoUri: string): Promise<Boolean> {
-  let endpoint = getValueFor('endpoint');
-  let token = await getToken();
+  let endpoint = await getValueFor('endpoint');
+  let token = await getValueFor('token');
   let user_id = getValueFor('user_id');
 
   const uploadId = (Math.random() + 1).toString(36).substring(6);
@@ -144,3 +151,7 @@ export async function uploadPhotoToAlbum(albumUIDs: Array<string>, photoUri: str
 
     return uploadPromise;
   } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
