9854aed35fd425f8565ee3860b97090c6e10b83d
newtondotcom <asphalt9fr@gmail.com>
2024-06-18 16:00:00 +0000

    rename auth screen

---
+++ app/(tabs)/auth.tsx
@@ -0,0 +1,15 @@
+import Auth from "@/components/photoprism/auth";
+import {
+  Heading,
+  View 
+} from '@gluestack-ui/themed';
+import React from "react";
+
+export default function App() {
+  return (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Heading>Connect to your server</Heading>
+    <Auth />
+  </View>
+  )
+}
--- app/(tabs)/index.tsx
+++
@@ -1,5 +0,0 @@
-import Auth from "@/components/photoprism/auth";
-
-export default function App() {
-  return <Auth />;
-}
