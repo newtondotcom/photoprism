19e6634e52b9fcc05b24fb247904d60c90a1dbf6
newtondotcom <asphalt9fr@gmail.com>
2024-06-18 16:00:00 +0000

    auth component

--- components/photoprism/auth.tsx
+++ components/photoprism/auth.tsx
@@ -1,37 +1,34 @@
 import { Button, TextInput, View } from "react-native";
-import * as SecureStore from 'expo-secure-store';
-
-
-async function save(key, value) {
-    await SecureStore.setItemAsync(key, value);
-}
-
-async function getValueFor(key) {
-    let result = await SecureStore.getItemAsync(key);
-    if (result) {
-      alert("🔐 Here's your value 🔐 \n" + result);
-    } else {
-      alert('No values stored under that key.');
-    }
-}
+import {getToken} from '@/src/photoprism';
 
 export default function Auth() {
 
-    const [text, setText] = useState('');
+    const [host, setHost] = useState('');
     const [port, setPort] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [state, setState] = useState(false);
 
+    async function connect() {
+        const token = await getToken();
+    }
 
-    return (
-        <View>
-            <TextInput placeholder="Endpoint" value={text}/>
-            <TextInput placeholder="Port" value={port}/>
-            <TextInput placeholder="Username" value={username}/>
-            <TextInput placeholder="Password" value={username}/>
-            // SSL IS FALSE
-            <Button title="Connect" />
-
-        </View> 
-    );
+    if (state) { 
+        return (
+            <View>
+                <TextInput placeholder="Endpoint" value={host}/>
+                <TextInput placeholder="Port" value={port}/>
+                <TextInput placeholder="Username" value={username}/>
+                <TextInput placeholder="Password" value={password}/>
+                // SSL IS FALSE by default
+                <Button title="Connect" onClick={connect} />
+            </View> 
+        );
+    } else {
+        return (
+            <View>
+                <Text>You are connected to Photoprism</Text>
+            </View> 
+        );        
+    }
 }
