02596ad51169bab6310c54f7e925466624daa659
newtondotcom <asphalt9fr@gmail.com>
2024-06-18 16:00:00 +0000

    rewrote all sync logic

--- app/(tabs)/sync.tsx
+++ app/(tabs)/sync.tsx
@@ -1,13 +1,19 @@
 import {useEffect, useState } from "react";
-import { View, Text, Button, Alert } from "react-native";
+import { View, Text, Button} from "react-native";
 import * as MediaLibrary from "expo-media-library";
 import {
   uploadPhotoToAlbum,
   getPhotos,
 } from "@/scripts/photoprism";
-import { AlbumExpo, Asset ,PagedInfo} from "@/scripts/types/expo";
+import { Asset ,PagedInfo} from "@/scripts/types/expo";
 import { getValueFor } from "@/scripts/store";
-import { PhotoPrismOrder, SearchPhotos } from "@/scripts/types/photoprism";
+import { PhotoPrismMergedPhoto, SearchPhotos } from "@/scripts/types/photoprism";
+import {
+  Alert,
+  AlertIcon,
+  AlertText,
+  InfoIcon}
+from '@gluestack-ui/themed';
 
 export default async function App() {
 
@@ -15,6 +21,11 @@ export default async function App() {
   const [token, setToken] = useState("");
   const [user_id, setUserId] = useState("");
   const [albumUID, setAlbumUID] = useState("");
+  const [localAssets, setLocalAssets] = useState([]);
+  const [prismAssets, setPrismAssets] = useState([]);
+  const [assetsToDelete, setAssetsToDelete] = useState([]);
+  const [assetsToUpload, setAssetsToUpload] = useState([]);
+  const count: number = 200;
 
   useEffect(() => {
     async function fetchConstants() {
@@ -22,9 +33,7 @@ export default async function App() {
         const fetchedEndpoint = await getValueFor("endpoint");
         const fetchedToken = await getValueFor("token");
         const fetchedUserId = await getValueFor("user_id");
-        const fetchedAlbumUID = await getValueFor("album_id");
-        
-        // Update state with fetched values
+        const fetchedAlbumUID = await getValueFor("album_id") | "";
         setEndpoint(fetchedEndpoint);
         setToken(fetchedToken);
         setUserId(fetchedUserId);
@@ -37,102 +46,113 @@ export default async function App() {
   
     fetchConstants();
   }, []);
+
+  async function fetchAssets(){
+    // Fetch all assets from the local library
+    const assets: Asset[] = await MediaLibrary.getAssetsAsync({ first: 10000 }).then(result => result.assets);
+    setLocalAssets(assets);
+    console.log(`Fetched ${assets.length} assets from local library`);
+  }
+
+  async function fetchPhotoPrism(){
+    // Initialize photos array
+    let photos: PhotoPrismMergedPhoto[] = [];
   
-
-  async function syncLibraryToAlbum(): Promise<string> {
-  const count: number = 200;
-
-  // Initialize photos array
-  let photos: PhotoPrismMergedPhoto[] = [];
-
-  // Fetch the photos from PhotoPrism in the specified album
-  const params: SearchPhotos = { count: count};
-  let photosFetched: PhotoPrismMergedPhoto[] = await getPhotos(params);
-  photos.push(...photosFetched);
-
-  // Keep fetching until all photos are retrieved
-  while (photosFetched.length === count) {
-    const offset = photos.length;
-    params.offset = offset;
-    photosFetched = await getPhotos(params);
+    // Fetch the photos from PhotoPrism in the specified album
+    const params: SearchPhotos = { count: count};
+    let photosFetched: PhotoPrismMergedPhoto[] = await getPhotos(params);
     photos.push(...photosFetched);
+  
+    // Keep fetching until all photos are retrieved
+    while (photosFetched.length === count) {
+      const offset = photos.length;
+      params.offset = offset;
+      photosFetched = await getPhotos(params);
+      photos.push(...photosFetched);
+      console.log(`Fetched ${photos.length} photos from PhotoPrism album`);
+    }
+
+    setPrismAssets(photos);
     console.log(`Fetched ${photos.length} photos from PhotoPrism album`);
   }
 
-  console.log(`Fetched ${photos.length} photos from PhotoPrism album`);
-
-  // Fetch all assets from the local library
-  const assets: Asset[] = await MediaLibrary.getAssetsAsync({ first: 10000 }).then(result => result.assets);
-
-  console.log(`Fetched ${assets.length} assets from local library`);
-
-  // Compute the deleted items (assets in PhotoPrism but not on phone)
-  const assetDeleted: PhotoPrismMergedPhoto[] = photos.filter(photo  => 
-    !assets.some(asset => 
+  function computeDiff(){
+      // Compute the deleted items (assets in PhotoPrism but not on phone)
+  const assetDeleted: PhotoPrismMergedPhoto[] = prismAssets.filter(photo  => 
+    !localAssets.some(asset => 
       asset.filename.includes(photo.OriginalName)
     )
   );
+  setAssetsToDelete(assetDeleted);
   if (assetDeleted.length > 0) {
     console.log(`Found ${assetDeleted.length} deleted assets`);
   } else {
     console.log(`No deleted assets found`);
   }
 
-  // Delete the missing assets from PhotoPrism
-  const deletedPhotosUIDs: string[] = assetDeleted.map(photo => photo.UID);
-  //await batchPhotosDelete(deletedPhotosUIDs);
-
   // Compute the missing elements (assets on phone but not in PhotoPrism)
-  const missingAssets: Asset[] = assets.filter(asset =>
-    !photos.some(photo =>
+  const missingAssets: Asset[] = localAssets.filter(asset =>
+    !prismAssets.some(photo =>
       asset.filename.includes(photo.OriginalName)
     )
   );
-
+  setAssetsToUpload(missingAssets);
   if (missingAssets.length === 0 && missingAssets.length === 0) {
     console.log(`No missing assets found`);
     return 'ok';
   } else {
     console.log(`Found ${missingAssets.length} missing assets to upload`);
   }
+  }
+
+  // Delete the assets only present on phone from PhotoPrism
+  async function deleteAssets(){
+    const deletedPhotosUIDs: string[] = assetsToDelete.map(photo => photo.UID);
+    //await batchPhotosDelete(deletedPhotosUIDs);
+  }
+
+  // Upload the missing assets missing from PhotoPrism  
+  async function uploadAssetsToPhotoSync(): Promise<string> {
 
   // Find the best batch size
-  let bestBatchSize = 1;
-  let previousBatchTimePerFile = Infinity;
-  let currentBatchSize = 1;
-  let batchSizeLimit = missingAssets.length;
+  let bestBatchSize: number = 1;
+  let previousBatchTimePerFile: number = Infinity;
+  let batchTimePerFile: number = 1;
+  let currentBatchSize: number = 1;
+  const batchSizeLimit: number = assetsToUpload.length;
+  let indexUploaded: number = 0;
 
-  while (currentBatchSize <= batchSizeLimit) {
-    const startTime = Date.now();
+  while (batchTimePerFile < previousBatchTimePerFile && currentBatchSize <= batchSizeLimit) {
+    const startTime: number = Date.now();
 
-    for (let i = 0; i < missingAssets.length; i += currentBatchSize) {
-      const batchAssets: Asset[] = missingAssets.slice(i, i + currentBatchSize);
-      const uploadPromises = batchAssets.map(asset => uploadPhotoToAlbum([albumUID], asset.uri, asset , endpoint, token, user_id));
-      await Promise.all(uploadPromises);
-    }
+    const batchAssets: Asset[] = assetsToUpload.slice(indexUploaded, indexUploaded + currentBatchSize);
+    const uploadPromises = batchAssets.map(asset => uploadPhotoToAlbum([albumUID], asset.uri, asset, endpoint, token, user_id));
+    await Promise.all(uploadPromises);
+    indexUploaded += currentBatchSize;
 
-    const endTime = Date.now();
-    const batchDuration = endTime - startTime;
-    const batchTimePerFile = batchDuration / missingAssets.length;
+    const endTime: number = Date.now();
+    const batchDuration: number = endTime - startTime;
+    batchTimePerFile = batchDuration / batchAssets.length;
 
-    console.log(`Batch size: ${currentBatchSize}, Total time: ${batchDuration}ms, Time per file: ${batchTimePerFile}ms`);
+    console.log(`Batch size: ${currentBatchSize}, Total time: ${batchDuration} ms, Time per file: ${batchTimePerFile} ms`);
 
     if (batchTimePerFile > previousBatchTimePerFile) {
-      console.log(`###############################Optimal batch size found: ${bestBatchSize}`);
+      console.log(`############################### Optimal batch size found: ${bestBatchSize}`);
       break;
     } else {
       previousBatchTimePerFile = batchTimePerFile;
       bestBatchSize = currentBatchSize;
       currentBatchSize *= 2;
+      console.log(`############################### Not optimal batch size: ${bestBatchSize}`);
     }
   }
 
   console.log(`Best batch size: ${bestBatchSize}`);
 
   // Use the best batch size for the final upload
-  for (let i = 0; i < missingAssets.length; i += bestBatchSize) {
-    const batchAssets: Asset[] = missingAssets.slice(i, i + bestBatchSize);
-    const uploadPromises = batchAssets.map(asset => uploadPhotoToAlbum([albumUID], asset.uri, asset , endpoint, token, user_id));
+  for (let i = indexUploaded; i < assetsToUpload.length; i += bestBatchSize) {
+    const batchAssets: Asset[] = assetsToUpload.slice(i, i + bestBatchSize);
+    const uploadPromises = batchAssets.map(asset => uploadPhotoToAlbum([albumUID], asset.uri, asset, endpoint, token, user_id));
     await Promise.all(uploadPromises);
   }
 
@@ -142,7 +162,8 @@ export default async function App() {
 }
 
   async function syncLibrary() {
-    await syncLibraryToAlbum();
+    await deleteAssets();
+    await uploadAssetsToPhotoSync();
   }
 
   async function test() {
@@ -167,6 +188,18 @@ export default async function App() {
       <View style={{ marginVertical: 50 }}>
         <Button title="Sync Library" onPress={syncLibrary} />
         <Button title="test" onPress={test} />
+        <Alert mx="$2.5" action="info" variant="solid">
+  <AlertIcon as={InfoIcon} mr="$3" />
+  <AlertText>
+    {assetsToDelete.length} have been deleted from your phone
+  </AlertText>
+</Alert>
+<Alert mx="$2.5" action="info" variant="solid">
+  <AlertIcon as={InfoIcon} mr="$3" />
+  <AlertText>
+    {assetsToUpload.length} need to be uploaded to PhotoPrism
+  </AlertText>
+</Alert>
       </View>
     </View>
   );
