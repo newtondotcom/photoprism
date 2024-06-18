0b896a10c130513a57263de9c8f753462e785826
newtondotcom <asphalt9fr@gmail.com>
2024-06-18 16:00:00 +0000

    method on Change

--- components/photoprism/auth.tsx
+++ components/photoprism/auth.tsx
@@ -16,10 +16,10 @@ export default function Auth() {
     if (state) { 
         return (
             <View>
-                <TextInput placeholder="Endpoint" value={host}/>
-                <TextInput placeholder="Port" value={port}/>
-                <TextInput placeholder="Username" value={username}/>
-                <TextInput placeholder="Password" value={password}/>
+                <TextInput placeholder="Endpoint" onChangeText={setHost} value={host}/>
+                <TextInput placeholder="Port" onChangeText={setPort} value={port}/>
+                <TextInput placeholder="Username" onChangeText={setUsername} value={username}/>
+                <TextInput placeholder="Password" onChangeText={setPassword} value={password}/>
                 // SSL IS FALSE by default
                 <Button title="Connect" onClick={connect} />
             </View> 
