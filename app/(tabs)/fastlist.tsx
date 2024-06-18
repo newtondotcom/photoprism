c4d7918fee652c6ac0c6c6722380c2e98f34c777
newtondotcom <asphalt9fr@gmail.com>
2024-06-18 16:00:00 +0000

    fastlist example component

---
+++ app/(tabs)/fastlist.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, StatusBar } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+
+const DATA = [
+  {
+    title: "First Item",
+  },
+  {
+    title: "Second Item",
+  },
+];
+
+export default function MyList () {
+  return (
+    <FlashList
+      data={DATA}
+      renderItem={({ item }) => <Text>{item.title}</Text>}
+      estimatedItemSize={200}
+    />
+  );
+};
